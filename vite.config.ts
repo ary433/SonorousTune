7cf904a41ba135b3eebd6582eeecd09391e4b30f
himanshu-sugha <143184981+himanshu-sugha@users.noreply.github.com>
2025-01-01 18:40:38 +0530

    refactor: update project name and configuration to SonorousTune

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+/// <reference types="vite-plugin-svgr/client" />
+export default defineConfig({
+  plugins: [svgr(), react(), basicSsl(), nodePolyfills(), tsconfigPaths()],
+  base: 'https://sonoroustune.github.io/sonoroustune-telegram-web-app/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
